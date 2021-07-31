a7f9fdc82464629b4a0c7972764a55c60269de06
김태섭 <kyoer234@gmail.com>
2021-07-31 10:22:57 +0900

    Fragment 두개 추가 (단순작업)

---
+++ app/src/main/java/com/nepplus/viewpager_20210731/fragments/MyinfoFragment.kt
@@ -0,0 +1,18 @@
+package com.nepplus.viewpager_20210731.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nepplus.viewpager_20210731.R
+
+class MyinfoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_info,container,false)
+    }
+}
