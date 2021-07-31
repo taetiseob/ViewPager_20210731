474580f112946c482da2bcd00fcd8b94889e77e4
김태섭 <kyoer234@gmail.com>
2021-07-31 11:13:35 +0900

    상황별로 다른 값 리턴되는 when 활용 예시

--- app/src/main/java/com/nepplus/viewpager_20210731/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/nepplus/viewpager_20210731/adapters/MainViewPagerAdapter.kt
@@ -13,22 +13,18 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
     }
 
     override fun getItem(position: Int): Fragment {
-        if(position== 0){
-            return NameFragment()
-        }else if(position == 1){
-            return MyinfoFragment()
-        }else{
-            return HelloFragment()
+        return when( position ) {
+            0 -> NameFragment()
+            1 -> MyinfoFragment()
+            else -> HelloFragment()
         }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        if(position === 0){
-            return "이름"
-        }else if(position === 1){
-            return "정보"
-        }else{
-            return "인사말"
+        return when(position){
+            0 -> "이름"
+            1 -> "정보"
+            else -> "인삿말"
         }
     }
 }
