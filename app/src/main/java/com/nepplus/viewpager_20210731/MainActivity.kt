ec85d0813700b12902ddf0cf093d22c9a5f16b75
김태섭 <kyoer234@gmail.com>
2021-07-31 10:11:06 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/viewpager_20210731/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.viewpager_20210731
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
